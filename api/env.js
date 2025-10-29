0a6cb4b280055c05a00cc4d99cd25c83657506fd
Tester Inet Doc <tester.inet.doc@gmail.com>
2025-10-29 14:10:19 +0700

    last

--- api/env.js
+++ api/env.js
@@ -6,7 +6,11 @@
 // export const url_env = "https://202.137.147.218:8443"
 // export const cookie_env = ".AspNetCore.Session=CfDJ8C3oQRBSug5PixF%2Fewk2lMvzP%2BKnfrujIBofU7Gwu4ih03WjfHW3WoRHBY4bLw25Qs5zJq61u2%2Bt%2BPl0fI9ACvJWswbfCbuM85bYsCF6Q0DWI1FDyHxLj%2FgptL5Xi7nNnOVAtNGJfdARkj6hDtJDYHd3lh800cRfvqvwWQk%2BYbba"
 
-export const url_env = "https://202.137.144.8"
-export const cookie_env = ".AspNetCore.Antiforgery.7AhjkVvrl1g=CfDJ8C3oQRBSug5PixF%2Fewk2lMvlh7mqt%2F2kwzrszoYlF0o8KzcwRVJ94beRfAM2jMUUm5lTE8qmDudPyaifRClZ7xL2krq1MgMNrGOOTxDkC1k2pai9Mb%2FruohAh4uFulqVNB1V38oLKUnUOUvrGGSyuq0s%2F2QSH5Mc%2BtXI%2FHPO2jAF"
-//export const url_env = "https://114.129.24.98"
-//export const cookie_env = ".AspNetCore.Antiforgery.7AhjkVvrl1g=CfDJ8C3oQRBSug5PixF%2Fewk2lMslALPjy0mZnApBfo%2BPoGp%2BtbRB%2FqkSnzYsA%2F6bg3mka4aET6h3YwhFC4ljaIQZrInJM%2BmN68MAVxMFkE%2B6smWcEVq4WBb0s%2BOHIk8DfxhQ97rYBhQT3bGWQm8Cem0w9xgMme6ir3T%2FEvE5gbpZsca1"
+//export const url_env = "https://202.137.144.8"
+//export const cookie_env = ".AspNetCore.Antiforgery.7AhjkVvrl1g=CfDJ8C3oQRBSug5PixF%2Fewk2lMvlh7mqt%2F2kwzrszoYlF0o8KzcwRVJ94beRfAM2jMUUm5lTE8qmDudPyaifRClZ7xL2krq1MgMNrGOOTxDkC1k2pai9Mb%2FruohAh4uFulqVNB1V38oLKUnUOUvrGGSyuq0s%2F2QSH5Mc%2BtXI%2FHPO2jAF"
+export const url_env = "https://114.129.24.98"
+export const cookie_env = ".AspNetCore.Antiforgery.7AhjkVvrl1g=CfDJ8C3oQRBSug5PixF%2Fewk2lMslALPjy0mZnApBfo%2BPoGp%2BtbRB%2FqkSnzYsA%2F6bg3mka4aET6h3YwhFC4ljaIQZrInJM%2BmN68MAVxMFkE%2B6smWcEVq4WBb0s%2BOHIk8DfxhQ97rYBhQT3bGWQm8Cem0w9xgMme6ir3T%2FEvE5gbpZsca1"
+
+// Gm@soudaphone.com
+// apisit@soudaphone.com
+// souliya@soudaphone.com
